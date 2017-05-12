7ef73dc694d9696303c01e8b93d1167eac0a2665
lancegab <lslabrague@up.edu.ph>
2017-05-12 16:31:54 +0800

    initial files

---
+++ index.js
@@ -0,0 +1,94 @@
+const express = require('express');
+const bodyparser = require('body-parser');
+const cookieparser = require('cookie-parser');
+const session = require('express-session');
+const flash = require('express-flash');
+const consolidate = require('consolidate');
+const passport = require('./config/passport');
+const database = require('./database');
+const User = require('./models').User;
+const Account = require('./models').Account;
+
+const app = express();
+
+app.engine('html', consolidate.nunjucks);
+app.set('views', './views');
+
+app.use(bodyparser.urlencoded({
+	extended: true
+}));
+
+app.use(cookieparser('secret-cookie'));
+app.use(session({ resave: false, saveUninitialized: false, secret: 'secret-cookie' }));
+app.use(flash());
+app.use(passport.initialize());
+
+app.use('/static', express.static('./static'));
+app.use(require('./auth-routes'));
+
+app.get('/', function(req, res) {
+	res.render('index.html');
+});
+
+app.post('/signup', function(req, res){
+	const name = req.body.name;
+	console.log(req.body);
+});
+
+app.get('/profile', requireSignedIn, function(req, res) {
+	const email = req.session.currentUser;
+	User.findOne({ where: { email: email } }).then(function(user) {
+		res.render('profile.html', {
+			user: user
+		});
+	});
+});
+
+app.post('/transfer', requireSignedIn, function(req, res) {
+	const recipient = req.body.recipient;
+	const amount = parseInt(req.body.amount, 10);
+
+	const email = req.session.currentUser;
+	User.findOne({ where: { email: email } }).then(function(sender) {
+		User.findOne({ where: { email: recipient } }).then(function(receiver) {
+			Account.findOne({ where: { user_id: sender.id } }).then(function(senderAccount) {
+				Account.findOne({ where: { user_id: receiver.id } }).then(function(receiverAccount) {
+					database.transaction(function(t) {
+						return senderAccount.update({
+							balance: senderAccount.balance - amount
+						}, { transaction: t }).then(function() {
+							return receiverAccount.update({
+								balance: receiverAccount.balance + amount
+							}, { transaction: t });
+						});
+					}).then(function() {
+						req.flash('statusMessage', 'Transferred ' + amount + ' to ' + recipient);
+						res.redirect('/profile');
+					});
+				});
+			});
+		});
+	});
+});
+
+app.get('/auth/twitter', passport.authenticate('twitter'));
+app.get('/auth/twitter/callback',
+    passport.authenticate('twitter', {
+        failureRedirect: '/'
+    }),
+    function(req, res) {
+        req.session.currentUser = req.user.email;
+        res.redirect('/profile');
+    }
+);
+
+function requireSignedIn(req, res, next) {
+    if (!req.session.currentUser) {
+        return res.redirect('/');
+    }
+    next();
+}
+
+app.listen(3000, function() {
+	console.log('Server is now running at port 3000');
+});
---
+++ models.js
@@ -0,0 +1,49 @@
+const Sequelize = require('sequelize');
+const database = require('./database');
+
+const User = database.define('users', {
+
+	id: {
+		type: Sequelize.INTEGER,
+		primaryKey: true,
+		autoIncrement: true,
+		allowNull: false
+	},
+	email: {
+		type: Sequelize.STRING,
+		unique: true,
+		allowNull: false
+	},
+	password: {
+		type: Sequelize.STRING,
+		allowNull: false
+	}
+}, {
+	timestamps: true
+});
+
+const Account = database.define('accounts', {
+    id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+    },
+    balance: {
+        type: Sequelize.FLOAT,
+        defaultValue: 0
+    },
+    user_id: {
+        type: Sequelize.INTEGER,
+        references: {
+            model: 'users',
+            key: 'id'
+        }
+    }
+}, {
+    timestamps: true
+});
+
+module.exports.User = User;
+module.exports.Account = Account;
+database.sync();
